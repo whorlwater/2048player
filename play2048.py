94e20d7b0e8dfc10d5b5ee0a226791f56be2ff4b
Snail <snail@solitarysnail.com>
2014-03-20 18:50:58 -0700

    Added preferred method and cleaned up.

--- play2048.py
+++ play2048.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from random import choice
 import argparse
 
+
 class Session(object):
     def __init__(self):
         self.browser = webdriver.Firefox()
@@ -39,13 +40,27 @@ class Session(object):
         else:
             return False
 
-    def get_details(self, xpath, element_type, parameter=None):
+    def get_details(self, xpath, element_type, how_many, parameter=None):
         if element_type == 'text':
-            return self.browser.find_element_by_xpath(xpath).text
+            if how_many == 'first':
+                return self.browser.find_element_by_xpath(xpath).text
+            elif how_many == 'all':
+                list_of_details = self.browser.find_elements_by_xpath(xpath)
+                list_of_details_text = [detail.text for detail in list_of_details]
+                return list_of_details_text
+            else:
+                critical_error('The total of get_details() can only be "first" or "all".')
         elif element_type == 'attribute':
-            return self.browser.find_element_by_xpath(xpath).get_attribute(parameter)
+            if how_many == 'first':
+                return self.browser.find_element_by_xpath(xpath).get_attribute(parameter)
+            elif how_many == 'all':
+                list_of_details = self.browser.find_elements_by_xpath(xpath)
+                list_of_details_attributes = [detail.get_attribute(parameter) for detail in list_of_details]
+                return list_of_details_attributes
+            else:
+                critical_error('The total of get_details() can only be "first" or "all".')
         else:
-            print 'ERROR IN GET_ELEMENT: element_type can only be "text" or "attribute".'
+            critical_error('The element_type of get_details() can only be "text" or "attribute".')
             return None
 
 
@@ -58,7 +73,7 @@ def critical_error(message):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('method', help='Choose random or ordered method of play.', choices=['random','ordered'])
+    parser.add_argument('method', help='Choose random, ordered, or preferred method of play.', choices=['random','ordered','preferred'])
     args = parser.parse_args()
     args = vars(args)
 
@@ -66,32 +81,74 @@ def main():
     current_session.browser.get('http://gabrielecirulli.github.io/2048/')
     play_2048(current_session, current_session.browser, args)
 
+
 def play_2048(session, browser, args):
     xpath = {
-    'gameboard' : '//div[@class="game-container"]',
-    'gameover' : '//div[@class="game-message game-over"]',
-    'retry' : '//a[@class="retry-button"]'
+    'gameboard'     : '//div[@class="game-container"]',
+    'gameover'      : '//div[@class="game-message game-over"]',
+    'retry'         : '//a[@class="retry-button"]',
+    'cell_contents' : '//div[@class="tile-inner"]',
+    'new_tiles'     : '//div[contains(@class, "tile-new")]',
+    'merged_tiles'  : '//div[contains(@class, "tile-merged")]'
     }
 
     command_list = [Keys.ARROW_UP,Keys.ARROW_RIGHT,Keys.ARROW_DOWN,Keys.ARROW_LEFT]
 
+    up    = command_list[0]
+    right = command_list[1]
+    down  = command_list[2]
+    left  = command_list[3]
+
     session.click_on_element(xpath['gameboard'])
 
+    def retry():
+        session.click_on_element(xpath['retry'])
+
+    def gameboard_exists():
+        return session.element_exists(xpath['gameboard'])
+
+    def game_is_over():
+        return session.element_exists(xpath['gameover'])
+
+    def move(command):
+        session.type_keys(xpath['gameboard'],command)
+
+    def new_tiles():
+        return session.get_details(xpath['new_tiles'], 'attribute', 'all', 'class')
+
+    def merged_tiles():
+        return session.get_details(xpath['merged_tiles'], 'attribute', 'all', 'class')
+
     if args['method'] == 'random':
-        while session.element_exists(xpath['gameboard']) is True:
-            if session.element_exists(xpath['gameover']) is not True:
+        while gameboard_exists() is True:
+            if game_is_over() is not True:
                 command = choice(command_list)
-                session.type_keys(xpath['gameboard'],command)
+                move(command)
             else:
-                session.click_on_element(xpath['retry'])
+                retry()
 
     elif args['method'] == 'ordered':
-        while session.element_exists(xpath['gameboard']) is True:
-            if session.element_exists(xpath['gameover']) is not True:
+        while gameboard_exists() is True:
+            if game_is_over() is not True:
                 for command in command_list:
-                    session.type_keys(xpath['gameboard'],command)
+                    move(command)
             else:
-                session.click_on_element(xpath['retry'])
+                retry()
+
+    elif args['method'] == 'preferred':
+        while gameboard_exists() is True:
+            if game_is_over() is not True:
+                initial_new_tiles = new_tiles()
+                initial_merged_tiles = merged_tiles()
+                move(down)
+                if new_tiles() == initial_new_tiles and merged_tiles() == initial_merged_tiles:
+                    move(left)
+                    if new_tiles() == initial_new_tiles and merged_tiles() == initial_merged_tiles:
+                        move(right)
+                        if new_tiles() == initial_new_tiles and merged_tiles() == initial_merged_tiles:
+                            move(up)
+            else:
+                retry()
 
     else:
         critical_error('Game method not available option. Check method argument choices against actual game methods.')
